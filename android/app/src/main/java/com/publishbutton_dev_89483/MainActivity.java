a587da37eb426dd027cee8d4f2f0a5c26f6862ff
Crowdbotics <engineering@crowdbotics.com>
2023-06-07 11:05:15 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/publishbutton_dev_89483/MainActivity.java
@@ -0,0 +1,15 @@
+package com.publishbutton_dev_89483;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "publishbutton_dev_89483";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "publishbutton-dev-89483",
+  url: "https://publishbutton-dev-89483.botics.co",
+  api: "https://publishbutton-dev-89483.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
